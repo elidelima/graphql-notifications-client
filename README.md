2ba48db2f80ba70b07c47c255aca6b99b34af0eb
elidelima <eli.lima@alpar.com.br>
2018-09-25 19:10:51 -0300

    automatizing structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # notifications-client
+
+
+#Step 1: Install Gulp and some plug-ins
+npm install -g gulp
+npm install gulp gulp-sass
+
+
---
+++ gulp-tasks/gulpfile.js
@@ -0,0 +1,13 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('../src/sass/*.scss')
+            .pipe(sass())
+        .pipe(gulp.dest('../src/css'))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('../src/sass/*.scss', ['sass']);
+})
